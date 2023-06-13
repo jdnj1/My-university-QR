16063c2dab1054ba87b41dc92b794bba27aeb948
Jose Daniel Neme Jerez <jdnj1@gcloud.ua.es>
2023-06-13 13:02:09 +0200

    iniciar angular en el proyecto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# TFG
+# My University QR
+
+Este proyecto ha sido generado con [Angular CLI](https://github.com/angular/angular-cli) versión 16 a partir de la plantilla .
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthLayoutComponent,
+    AdminLayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
