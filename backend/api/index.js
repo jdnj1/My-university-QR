0e6949803434902be036670c8dd237793f8dff55
Jose Daniel Neme Jerez <jdnj1@gcloud.ua.es>
2023-06-13 12:04:45 +0200

    env y cors

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,10 +3,22 @@ Importación de módulos
 */
 
 const express = require('express');
-// Crear una aplicación de express
+const cors = require('cors');
+require('dotenv').config();
+
+//Crear una aplicación de express
 const app = express();
 
-// Abrir la aplicacíon en el puerto 3000
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto ' + 3000);
+app.use(cors());  
+
+app.listen(process.env.PORT, () => {
+    console.log('Servidor corriendo en el puerto ', process.env.PORT);
+});
+
+
+app.get('/', (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'Hola mundo'
+    });
 });
