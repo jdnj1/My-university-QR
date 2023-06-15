e9f35f9b96a763cf5b8b34f62f4d69881f5be880
Jose Daniel Neme Jerez <jdnj1@gcloud.ua.es>
2023-06-15 12:50:24 +0200

    Prueba de consexión a base de datos mysql

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,12 +4,41 @@ Importación de módulos
 
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql');
 require('dotenv').config();
 
-//Crear una aplicación de express
+// Crear una aplicación de express
 const app = express();
 
 app.use(cors());  
+
+// Prueba de coneción con base de datos
+const conect = mysql.createConnection({
+    host: 'localhost',
+    database: 'prueba_tfg',
+    user: 'root',
+    password: ''
+});
+
+try{
+    conect.connect();
+    console.log("Conexión exitosa");
+}
+catch(err){
+    console.error( err );
+
+}
+
+conect.query('SELECT * FROM user', function (error, results, fields) {
+    if (error) throw error;
+
+    // connected!
+    results.forEach(result => {
+        console.log(result);
+    });
+  });
+
+conect.end();
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en el puerto ', process.env.PORT);
