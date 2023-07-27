6525c67e5c6d4c2751df84aa536e5eca4c38ca26
Jose Daniel Neme Jerez <jdnj1@gcloud.ua.es>
2023-07-27 20:35:34 +0200

    conmtroller y route de smartUniversity

---
+++ backend/api/controllers/smartuni.js
@@ -0,0 +1,82 @@
+/**
+ * CONTROLLER: SMARTUNIVERSITY
+ */
+
+// === Importar
+
+const axios = require('axios');
+
+// Propio
+const {dbConsult} = require('../database/db');
+const { response } = require('express'); // Response de Express
+const bcrypt = require('bcryptjs'); // BcryptJS
+
+/**
+ * Devuelve los datos de SmartUniversity comprendidos en las fechas y filtros proporcionados
+ * 
+ * @param {*} req Peticion del cliente.
+ * @param {*} res Respuesta a enviar por el servidor.
+ */
+const getData = async( req , res ) => {
+    try {
+        let token = req.query.token;
+
+        axios.post(`${process.env.URLSU}/${token}/getData`, req.body)
+            .then((result) => {
+                console.log(result.data)
+                res.status(200).json({
+                    res: result.data
+                });
+                return;
+        
+            }).catch((err) => {
+                console.log(err)
+                res.status(400).json({
+                    res: err
+                });
+                return;
+            });
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            msg: 'Error al intentar devolver los datos'
+        });
+    }
+}
+
+/**
+ * Devuelve un maximo, minimo o el utimo dato de SmartUniversity comprendido en las fechas y filtros proporcionados
+ * 
+ * @param {*} req Peticion del cliente.
+ * @param {*} res Respuesta a enviar por el servidor.
+ */
+const getDataOperation = async( req , res ) => {
+    try {
+        let {token, dateFrom, dateTo, operation, uid, name} = req.query;
+
+        axios.get(`${process.env.URLSU}/${token}/time_start/${dateFrom}/time_end/${dateTo}/operation/${operation}/uid/${uid}/name/${name}/getDataOperation`)
+            .then((result) => {
+                console.log(result.data)
+                res.status(200).json({
+                    res: result.data
+                });
+                return;
+            
+            }).catch((err) => {
+                console.log(err)
+                return;
+            });
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            msg: 'Error al intentar devolver los datos'
+        });
+    }
+}
+
+
+module.exports = {getData, getDataOperation};
