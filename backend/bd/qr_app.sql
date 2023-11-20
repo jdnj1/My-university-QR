4a2f9bdae57fc61f07ba1d409f18d181762a4ead
Jose Daniel Neme Jerez <jdnj1@gcloud.ua.es>
2023-11-20 12:31:26 +0100

    sql base de datos

--- backend/bd/qr_app.sql
+++ backend/bd/qr_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-11-2023 a las 01:09:21
+-- Tiempo de generación: 20-11-2023 a las 12:30:43
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -52,17 +52,7 @@ INSERT INTO `consult` (`idConsult`, `name`, `token`, `typeDate`, `dateFrom`, `da
 (1, 'Consumo eléctrico del sensor MLU00040001', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2ODk3NjQxOTF9.ZyZoVHLgFTwneop0pxn0yW059FUmTI92bnUIlklPHmQ', 0, '2023-05-19 03:00:00', '2023-05-19 05:00:00', 0, 1, '{\"uid\":\"MLU00040001\",\"name\":\"15m\"}', 4, 3, 1, 3, 0),
 (6, 'Gráfica de líneas con dos sensores', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2ODk3NjQxOTF9.ZyZoVHLgFTwneop0pxn0yW059FUmTI92bnUIlklPHmQ', 0, '2023-07-06 12:21:30', '2023-07-27 12:21:30', 0, 1, '{\"uid\":\"MLU00040001,MLU02000002\",\"name\":\"15m\"}', 1, 0, 1, 3, 2),
 (8, 'Gráfica de barras 2', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2ODk3NjQxOTF9.ZyZoVHLgFTwneop0pxn0yW059FUmTI92bnUIlklPHmQ', 0, '2023-07-05 22:00:00', '2023-07-06 22:00:00', 0, 1, '{\"uid\":\"MLU00080001,MLU00090001,MLU00200002\",\"name\":\"15m\"}', 1, 1, 1, 3, 1),
-(35, 'Nombre de la llamada', 'Token de la llamada', 0, '2023-09-22 11:40:20', '2023-10-07 11:40:20', 0, 1, '{}', 1, 0, 1, 6, 0),
-(36, 'Nombre de la llamada', 'Token de la llamada', 0, '2023-09-22 11:40:21', '2023-10-07 11:40:21', 0, 1, '{}', 1, 0, 1, 6, 1),
-(37, 'Nombre de la llamada', 'Token de la llamada', 0, '2023-09-22 11:40:22', '2023-10-07 11:40:22', 0, 1, '{}', 1, 0, 1, 6, 2),
-(38, 'Nombre de la llamada', 'Token de la llamada', 0, '2023-09-22 11:44:34', '2023-10-07 11:44:34', 0, 1, '{}', 1, 0, 1, 6, 3),
-(56, 'Pruebas', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2ODk3NjQxOTF9.ZyZoVHLgFTwneop0pxn0yW059FUmTI92bnUIlklPHmQ', 0, '2023-09-27 09:29:17', '2023-10-12 09:29:17', 0, 1, '{}', 1, 0, 0, 3, 3),
-(103, 'Nombre de la llamada', 'Token de la llamada', 0, '2023-11-12 20:57:40', '2023-11-27 20:57:40', 0, 0, '{}', 1, 0, 0, 3, 4),
-(104, 'Nombre de la llamada', 'Token de la llamada', 0, '2023-11-12 21:09:43', '2023-11-27 21:09:42', 0, 1, '{}', 1, 0, 0, 3, 5),
-(114, 'Nombre de la llamada', 'Token de la llamada', 0, '2023-11-13 00:12:02', '2023-11-20 00:12:02', 0, 1, '{}', 1, 0, 0, 257, 0),
-(115, 'Nombre de la llamada', 'Token de la llamada', 0, '2023-11-13 00:12:02', '2023-11-20 00:12:02', 0, 1, '{}', 1, 0, 0, 257, 1),
-(116, 'Nombre de la llamada', 'Token de la llamada', 0, '2023-11-13 00:12:02', '2023-11-20 00:12:02', 0, 1, '{}', 1, 0, 0, 257, 2),
-(118, 'Nombre de la llamada', 'Token de la llamada', 0, '2023-11-13 00:12:02', '2023-11-20 00:12:02', 0, 1, '{}', 1, 0, 0, 3, 6);
+(56, 'Pruebas', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2ODk3NjQxOTF9.ZyZoVHLgFTwneop0pxn0yW059FUmTI92bnUIlklPHmQ', 0, '2023-09-27 09:29:17', '2023-10-12 09:29:17', 0, 1, '{}', 1, 0, 0, 3, 3);
 
 -- --------------------------------------------------------
 
@@ -89,8 +79,6 @@ CREATE TABLE `qrcode` (
 INSERT INTO `qrcode` (`idQr`, `description`, `tagName`, `tagDescription`, `sizePrint`, `date`, `activated`, `share`, `user`) VALUES
 (3, 'Consumo electrónico', 'Primer QR', 'Prueba', 'a4', '2023-11-30', 1, 1, 10),
 (6, 'Se va a probar update en este código Qr', 'Postman fecha', 'admin prueba', 'a4', '2024-01-07', 1, 0, 10),
-(257, 'No deberia pasar', '', '', 'a4', '2023-11-25', 0, 0, 4),
-(258, 'dadaa', '', '', 'a4', '2023-12-09', 0, 0, 4),
 (265, 'Descripción del código QR', 'Nombre de etiqueta', 'pruebita', '\'a4\'', '2023-11-28', 0, 0, 4),
 (268, 'prueba solo insertId update', '', '', 'a4', '2023-12-10', 0, 0, 4);
 
@@ -167,7 +155,7 @@ ALTER TABLE `qrcode`
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- Restricciones para tablas volcadas
